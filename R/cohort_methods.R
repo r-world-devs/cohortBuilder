56cb88fa3e5dc51d0ee763776d0a0dfe7db797cc
Adam Forys <adam.forys@roche.com>
2024-10-30 15:31:38 +0100

    Add support for lenght 0 values in restore

--- R/cohort_methods.R
+++ R/cohort_methods.R
@@ -344,6 +344,7 @@ Cohort <- R6::R6Class(
           
           if (filter_state$type == "date_time_range") {
             filter_state$range <- na_fix(filter_state$range)
+            if (length(filter_state$range) == 0) filter_state$range <- NULL
             filter_state$range <- as.POSIXct(filter_state$range)
           }
           add_filter(
