e0e7499f168dd7e5ee5af9f326a23cdf92b73158
Dawid Borysiak <156790753+syroBx@users.noreply.github.com>
2025-01-20 20:28:40 +0100

    Update test-cohort_methods.R

--- tests/testthat/test-cohort_methods.R
+++ tests/testthat/test-cohort_methods.R
@@ -915,7 +915,7 @@ test_that("copy_step with run_flow trigger data calculations", {
 
   list_of_filters <- get_state(coh,coh$last_step_id())[[1]]$filters
 
-  expect_null(coh$get_data())
+  expect_null(get_data(coh))
 
   coh$copy_step(run_flow = TRUE)
 
