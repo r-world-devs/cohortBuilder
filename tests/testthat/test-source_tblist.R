b2868075ed2a15dcb17beec4e44e6bbb7bc1c81d
Borys <dawidborysiak02@gmail.com>
2025-04-22 20:52:47 +0200

    Add source tblist functions tests

--- tests/testthat/test-source_tblist.R
+++ tests/testthat/test-source_tblist.R
@@ -44,3 +44,81 @@ test_that("as.tblist works fine", {
   expect_s3_class(as.tblist(iris), "tblist")
 })
 
+test_that("filter_data in discrete filter works fine", {
+  test_var <- c("A", "B", NA, "C", "A", NA, "B")
+
+  test_data <- list(
+    test_dataset = data.frame(
+      var1 = test_var
+    )
+  )
+
+  # filter_data with keep_na = TRUE and value != NA
+  filter <- cb_filter.discrete.tblist(
+    variable = "var1", value = "A",
+    dataset = "test_dataset", keep_na = TRUE
+  )
+
+  result <- filter$filter_data(test_data)
+  expect_type(result$test_dataset$var1, "character")
+  expect_length(result$test_dataset$var1, length(test_var %>% .[. == "A"]))
+  expect_setequal(result$test_dataset$var1, c("A", NA))
+
+  # filter_data with keep_na = FALSE and value = NA
+  filter2 <- cb_filter.discrete.tblist(
+    variable = "var1", value = NA,
+    dataset = "test_dataset", keep_na = FALSE
+  )
+
+  result <- filter2$filter_data(test_data)
+  expect_type(result$test_dataset$var1, "character")
+  expect_length(result$test_dataset$var1, length(test_var %>% na.omit()))
+  expect_false(anyNA(result$test_dataset$var1))
+
+  # filter_data with keep_na = FALSE and value != NA
+  filter3 <- cb_filter.discrete.tblist(
+    variable = "var1", value = "B",
+    dataset = "test_dataset", keep_na = FALSE
+  )
+
+  result <- filter3$filter_data(test_data)
+  expect_type(result$test_dataset$var1, "character")
+  expect_length(result$test_dataset$var1, length(test_var %>% na.omit() %>% .[. == "B"]))
+  expect_setequal(result$test_dataset$var1, "B")
+})
+
+test_that("get_stats in discrete filter works fine", {
+  test_var <- c("A", "B", NA, "C", "A", NA, "B")
+
+  test_data <- list(
+    test_dataset = data.frame(
+      var1 = test_var
+    )
+  )
+
+  filter <- cb_filter.discrete.tblist(
+    variable = "var1", value = "A",
+    dataset = "test_dataset", keep_na = TRUE
+  )
+
+  result <- filter$get_stats(test_data, "n_data")
+  expect_identical(result, length(test_var %>% na.omit()))
+})
+
+test_that("plot_data in discrete filter works fine", {
+  test_var <- NULL
+
+  test_data <- list(
+    test_dataset = data.frame(
+      var1 = test_var
+    )
+  )
+
+  filter <- cb_filter.discrete.tblist(
+    variable = "var1", value = "A",
+    dataset = "test_dataset", keep_na = TRUE
+  )
+  filter$plot_data(test_data)
+  expect_silent(recordPlot())
+})
+
